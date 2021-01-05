208e8dfafc0d7daac07f8ef8717998c65c5c234f
Ben Lewis <bplewis5@gmail.com>
2021-01-04 19:06:21 -0600

    add service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -24,6 +25,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html"
         }),
+        new WorkboxPlugin.GenerateSW(),
         new MiniCssExtractPlugin({filename: '[name].css'})
     ],
     output: {
